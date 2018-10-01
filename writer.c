de7d0be55772aac6d87e416013da80eb4acfe4ce
Allison Bolen <allisonbolen@Allisons-MacBook-Pro.local>
2018-10-01 19:50:08 -0400

    initail

---
+++ writer.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <pthread.h>
+#include <string.h>
+
+#define SHM_SIZE 1024
+
+/**
+* This class is for practice with reading
+* and writing to shared memory
+*
+* @author Allison Bolen
+* @author Alec Allain
+* @version 9/28/18
+*/
+
+/** Global variables */
+pthread_t thread[50];
+int status;
+char userInput;
+int shmId;
+char* shmPtr;
+struct shmid_ds buffer;
+
+/**
+* Main method
+*/
+int main() {
+  int shmId;
+   char *shmPtr;
+
+   if ((shmId = shmget (IPC_PRIVATE, FOO, IPC_CREAT|S_IRUSR|S_IWUSR)) < 0) {
+      perror ("i can't get no..\n");
+      exit (1);
+   }
+   if ((shmPtr = shmat (shmId, 0, 0)) == (void*) -1) {
+      perror ("can't attach\n");
+      exit (1);
+   }
+   printf ("value a: %lu\t value b: %lu\n", (unsigned long) shmPtr, (unsigned long) shmPtr + FOO);
+   if (shmdt (shmPtr) < 0) {
+      perror ("just can't let go\n");
+      exit (1);
+   }
+   if (shmctl (shmId, IPC_RMID, 0) < 0) {
+      perror ("can't deallocate\n");
+      exit(1);
+   }
+
+   return 0; 
+}
