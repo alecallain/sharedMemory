ebc67055d5ca0c4fb32d8e14e6ded984f0341893
Allison Bolen <allisonbolen@Allisons-MacBook-Pro.local>
2018-10-01 21:09:59 -0400

    initail

--- reader.c
+++ reader.c
@@ -1,12 +1,5 @@
+
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
-#include <pthread.h>
-#include <string.h>#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
